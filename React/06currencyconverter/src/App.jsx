7df6644efff0ada3c4a43303efc2f56bfbfd7ffd
kirandas <cool.kdtech1234@gmail.com>
2024-10-06 01:02:58 +0530

    currency converter

---
+++ React/06currencyconverter/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import "./App.css";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
+import InputBox from "./components";
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
+
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
+
+  return (
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url(https://wallpaper.forfun.com/fetch/6c/6cf64de1460c2aea895a2d1953849832.jpeg)`,
+      }}
+    >
+      <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+        <form
+          onClick={(e) => {
+            e.preventDefault();
+            convert();
+          }}
+        >
+          <div className="w-full mb-1">
+            <InputBox
+              label="from"
+              amount={amount}
+              currencyOption={options}
+              onCurrencyChange={(currency) => setFrom(currency)}
+              onAmountChange={(amount) => setAmount(amount)}
+              selectedCurrency={form}
+            />
+          </div>
+          <div className="relative w-full h-0.5">
+            <button
+              className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+              onClick={swap}
+            >
+              Swap
+            </button>
+          </div>
+          <div className="w-full mb-1">
+            <InputBox
+              label="from"
+              amount={amount}
+              currencyOption={options}
+              onCurrencyChange={(currency) => setFrom(currency)}
+              onAmountChange={(amount) => setAmount(amount)}
+              selectedCurrency={form}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
