619ced311604de2293a366ee8dd1b02a38fcc8e2
kirandas <cool.kdtech1234@gmail.com>
2024-10-05 19:44:13 +0530

    tailwind bg changer

---
+++ React/04bgchanger/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  function changeColor(color){
+    setColor(color)
+  }
+
+  return (
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button 
+          onClick={()=>setColor('black')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg bg-neutral-600'>Black</button>
+          <button 
+          onClick={()=>setColor('blue')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg bg-blue-300'>Blue</button>
+          <button 
+          onClick={()=>setColor('yellow')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg bg-yellow-200'>Yellow</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
